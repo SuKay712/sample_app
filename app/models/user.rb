0bc96fa808bd9fca4c4918393f2ea54f51e876e5
Kay <khoile712003@gmail.com>
2024-07-10 14:31:52 +0700

    create Constant var to store attributes
    .
    
    .
    
    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
+
+  PERMITTED_ATTRIBUTES = %i(name email password password_confirmation birthday gender)
   before_save :downcase_email
 
   validates :password, presence: true, length: {minimum: Settings.password_length}
   validates :name, presence: true, length: {maximum: Settings.name_length}
-  validates :email, presence: true, length: {minimum: Settings.email_length_min, maximum: Settings.email_length_max},
+  validates :email, presence: true,
+    length: {minimum: Settings.email_length_min, maximum: Settings.email_length_max},
     format: {with: Settings.mail_regex},
     uniqueness: {case_sensitive: false, scope: :id}
-  validate :birthday_must_be_in_last_100_years, if: :birthday.present?
+  validates :gender, presence: true
+  validates :birthday, presence: true
+  validate :birthday_must_be_in_last_100_years, if: -> {birthday.present?}
 
   has_secure_password
 
@@ -18,7 +23,7 @@ class User < ApplicationRecord
 
   def birthday_must_be_in_last_100_years
     if birthday < 100.years.ago.to_date
-      errors.add(:birthday, t("users.birthday_at_least"))
+      errors.add :birthday, :birthday_at_least
     end
   end
 end
